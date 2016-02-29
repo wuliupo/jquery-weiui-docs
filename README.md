ed93be532101b05338a7b9984afa83bb3b644267
Liupo Wu <wu.liu-po@hp.com>
2016-02-29 14:52:20 +0800

    jQuery WeUI docs

---
+++ README.md
@@ -0,0 +1,13 @@
+# jQuery WeUI
+
+WeUI的jQuery版本，兼容jQuery和Zepto。
+
+jQuery WeUI 使用官方的CSS组件，加上一系列jQuery插件提供简洁强大的API。如果你使用过jQuery/Zepto，那么jQuery WeUI 没有任何上手难度。
+
+官网地址： [http://lihongxun945.github.io/jquery-weui/](http://lihongxun945.github.io/jquery-weui/)
+
+这是原 docs demo 的存档，使用 ```npm install``` 下载依赖库（jQuery，Bootstrap，WeUI）。
+
+# LICENSE
+
+[MIT](https://opensource.org/licenses/MIT)，尽情享受开源代码。
---
+++ docs.js
@@ -0,0 +1,124 @@
++ function($) {
+
+  "use strict";
+
+  var initSideMenu = function() {
+    var win = $(window),
+      body = $(document.body),
+      menu = $("#side-nav"),
+      windowHeight = $(window).height() / 3,
+      windowWidth = $(window).width(),
+      contentSection = $('.component'),
+      topCache = contentSection.map(function () { return $(this).offset().top; }),
+      device = $(".device");
+    var coder = new QRCode(document.getElementById("qrcode"), {
+      width: 200,
+      height: 200,
+    });
+    coder.makeCode(location.origin+device.find("iframe").attr("src"));
+
+    $(document).on("click", "#qrcode-btn", function() {
+      $("#qrcode").toggleClass("show");
+    });
+
+    device.initialLeft   = device.offset().left;
+    device.initialTop    = device.initialTop || device.offset().top;
+    device.dockingOffset = ($(window).height() - device.height()) / 2;
+    device.dockingOffset = (device.dockingOffset < -100 ? -100 : device.dockingOffset);
+
+    menu.initialLeft   = menu.offset().left;
+    menu.initialTop    = menu.initialTop || menu.offset().top;
+
+    var currentActive;
+
+    device.dockingOffset = (win.height() - device.height()) / 2;
+    device.dockingOffset = (device.dockingOffset < -100 ? -100 : device.dockingOffset);
+
+    var calculateScroll = function () {
+      // if small screen don't worry about this
+      if (windowWidth <= 768) {
+        return;
+      }
+
+      // Save scrollTop value
+      var contentSectionItem;
+      var currentTop = win.scrollTop();
+
+      // exit if no device
+      if (!device.length) {
+        return;
+      }
+
+      var top;
+      if ((device.initialTop - currentTop) <= device.dockingOffset) {
+        top = device.dockingOffset;
+        device[0].className = 'device device-fixed';
+        device.css({ top: top });
+      } else {
+        device[0].className = 'device';
+        device[0].setAttribute('style', '');
+      }
+
+      if ((menu.initialTop - currentTop) <= 30) {
+        top = 30;
+        menu[0].className = 'nav side-nav menu-fixed';
+        menu.css({ top: top });
+      } else {
+        menu[0].className = 'nav side-nav';
+        menu[0].setAttribute('style', '');
+      }
+
+      function updateContent(content) {
+        var $page = $('#window').html(content);
+      }
+
+      // Injection of components into device
+      for (var l = contentSection.length; l--;) {
+        if ((topCache[l] - currentTop) < windowHeight) {
+          if (currentActive === l) {
+            return;
+          }
+          currentActive = l;
+          body.find('.component.active').removeClass('active');
+          contentSectionItem = $(contentSection[l]);
+          contentSectionItem.addClass('active');
+          var id = contentSectionItem.attr('id');
+          menu.find(".active").removeClass("active");
+          if (id) {
+            device.attr('id', id + 'InDevice');
+            menu.find("a[href='#"+id+"']").parents("li").addClass("active");
+          } else {
+            device.attr('id', '');
+
+            //找到之前最近一个有id的
+            var prev = contentSectionItem.prev();
+            while(prev[0] && !prev.attr("id")) prev = prev.prev();
+            if(prev[0]) {
+              menu.find("a[href='#"+prev.attr("id")+"']").parents("li").addClass("active");
+            }
+          }
+          if (!contentSectionItem.hasClass('informational')) {
+			var hash = contentSectionItem.data("url");
+            var iframe = $("#window iframe").get(0);
+            if(iframe && hash) {
+              hash = '#' + hash;
+              if(iframe.src.indexOf(hash) === -1) {
+                iframe.contentWindow.document.location.hash = hash;
+                coder.makeCode(iframe.contentWindow.document.location.href);
+				$('#preview').attr('href', iframe.contentWindow.document.location.href);
+              }
+            }
+          }
+          break;
+        }
+      } 
+    }
+
+    win.on("scroll", calculateScroll);
+
+    calculateScroll();
+  };
+
+  initSideMenu();
+
+}($);
